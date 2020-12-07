edc6a5891bf55b98aa51164764233c566da713e9
rhoofr <rhoofr@yahoo.com>
2020-12-07 18:10:30 -0500

    Navbar and Roboto font

--- src/Palette.js
+++ src/Palette.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import Slider from 'rc-slider';
 import ColorBox from './ColorBox';
-import 'rc-slider/assets/index.css';
+import Navbar from './Navbar';
 import './Palette.css';
 
 class Palette extends Component {
@@ -21,17 +20,7 @@ class Palette extends Component {
     ));
     return (
       <div className='Palette'>
-        <div className='slider'>
-          <Slider
-            defaultValue={level}
-            min={100}
-            max={900}
-            step={100}
-            onAfterChange={this.changeLevel}
-          />
-        </div>
-
-        {/* Navbar goes here */}
+        <Navbar level={level} changeLevel={this.changeLevel} />
         <div className='Palette-colors'>{colorBoxes}</div>
         {/* footer eventually */}
       </div>
