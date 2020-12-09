0dbd751b5f08a547209a3347cb966ac2436b9c53
rhoofr <rhoofr@yahoo.com>
2020-12-09 14:10:49 -0500

    Add delete to PaletteForm

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -13,7 +13,7 @@ const styles = {
     marginBottom: '-3.5px',
     '&:hover svg': {
       color: 'white',
-      transform: 'scale(1.5)',
+      transform: 'scale(1.2)',
     },
   },
   boxContent: {
@@ -35,12 +35,12 @@ const styles = {
 };
 
 function DraggableColorBox(props) {
-  const { classes } = props;
+  const { classes, handleClick, name, color } = props;
   return (
-    <div className={classes.root} style={{ backgroundColor: props.color }}>
+    <div className={classes.root} style={{ backgroundColor: color }}>
       <div className={classes.boxContent}>
-        <span> {props.name}</span>
-        <DeleteIcon className={classes.deleteIcon} />
+        <span> {name}</span>
+        <DeleteIcon className={classes.deleteIcon} onClick={handleClick} />
       </div>
     </div>
   );
