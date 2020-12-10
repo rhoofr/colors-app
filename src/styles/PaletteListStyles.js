c5545f56f02c77e9e42f3e7a58e5362597308f8c
rhoofr <rhoofr@yahoo.com>
2020-12-10 18:08:37 -0500

    Add bg svg to PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,11 +1,18 @@
 import sizes from './sizes';
+import bg from './bg.svg';
 export default {
   root: {
-    backgroundColor: 'blue',
     height: '100vh',
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'center',
+    /* background by SVGBackgrounds.com */
+    backgroundColor: '#394bad',
+    backgroundImage: `url(${bg})`,
+    overflow: 'scroll',
+  },
+  heading: {
+    fontSize: '2rem',
   },
   container: {
     width: '50%',
@@ -35,13 +42,13 @@ export default {
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '2.5rem',
+    gridGap: '1.5rem',
     [sizes.down('md')]: {
       gridTemplateColumns: 'repeat(2, 50%)',
     },
     [sizes.down('xs')]: {
       gridTemplateColumns: 'repeat(1, 100%)',
-      gridGap: '1rem',
+      gridGap: '1.4rem',
     },
   },
 };
