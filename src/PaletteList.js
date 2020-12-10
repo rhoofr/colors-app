cb4c6f8872816ebee783fae93109275107344e19
rhoofr <rhoofr@yahoo.com>
2020-12-10 10:42:59 -0500

    MiniPalette delete

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -9,20 +9,22 @@ class PaletteList extends Component {
     this.props.history.push(`/palette/${id}`);
   }
   render() {
-    const { palettes, classes } = this.props;
+    const { palettes, classes, deletePalette } = this.props;
     return (
       <div className={classes.root}>
         <div className={classes.container}>
           <nav className={classes.nav}>
             <h1>React Colors</h1>
-            <Link to='./palette/new'>Create Palette</Link>
+            <Link to='/palette/new'>Create Palette</Link>
           </nav>
           <div className={classes.palettes}>
             {palettes.map((palette) => (
               <MiniPalette
-                key={palette.id}
                 {...palette}
                 handleClick={() => this.goToPalette(palette.id)}
+                handleDelete={deletePalette}
+                key={palette.id}
+                id={palette.id}
               />
             ))}
           </div>
